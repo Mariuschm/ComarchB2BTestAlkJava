a411564dcaf34845cddb35a80a55617774e0f3a0
Mariuschm <mariusz.midzio@gamil.com>
2023-05-28 10:27:32 +0200

    Added remotewebdriver

--- src/test/java/pl/alk/tests/BaseTest.java
+++ src/test/java/pl/alk/tests/BaseTest.java
@@ -1,11 +1,16 @@
 package pl.alk.tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.alk.pop.*;
 import pl.alk.utils.Config;
 import pl.alk.utils.Core;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /***
   Base class for other tests
@@ -20,8 +25,18 @@ public class BaseTest extends Core {
     private WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        driver = setDriver(Config.BROWSER);
+    public void setUp() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("browserVersion", "100");
+        chromeOptions.setCapability("platformName", "Windows");
+// Showing a test name instead of the session id in the Grid UI
+        chromeOptions.setCapability("se:name", "My simple test");
+// Other type of metadata can be seen in the Grid UI by clicking on the
+// session info or via GraphQL
+        chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value");
+        var grid = new URL("http://localhost:4444/");
+        driver = new RemoteWebDriver(grid,chromeOptions);
+        //driver = setDriver(Config.BROWSER);
         driver.get("https://demob2b-xl.comarch.pl/");
         //Maximize window
         driver.manage().window().maximize();
--- src/test/java/pl/alk/utils/Config.java
+++ src/test/java/pl/alk/utils/Config.java
@@ -7,7 +7,7 @@ public class Config {
             new File(System.getProperty("user.dir")).getAbsolutePath() + "/screenshots/";
     public static final String IMAGE_PATH_ERROR =
             new File(System.getProperty("user.dir")).getAbsolutePath() + "/screenshots/errors/";
-    public static final String BROWSER = "edge";
+    public static final String BROWSER = "firefox";
     public static final char CSV_SEPARATOR = ';';
     public static final String DOWNLOAD_PATH = new File(System.getProperty("user.home")).getAbsolutePath() + "/downloads/";
 
